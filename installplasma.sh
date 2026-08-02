f511d1eb68712575511c3a79ce72bcaf2c292c7d
coolnotlight3 <nonexistient@gmail.com>
2026-08-02 15:25:16 -0400

    Update installplasma.sh

--- installplasma.sh
+++ installplasma.sh
@@ -1,6 +1,6 @@
 sudo xbps-install kde-plasma kde-baseapps sddm xorg pulseaudio NetworkManager
-sudo ln /etc/sv/sddm /var/service
-sudo ln /etc/sv/dbus /var/service
-sudo ln /etc/sv/NetworkManager /var/service
+sudo ln -s /etc/sv/sddm /var/service
+sudo ln -s /etc/sv/dbus /var/service
+sudo ln -s /etc/sv/NetworkManager /var/service
 sudo rm /var/service/wpa_supplicant
 sudo reboot
