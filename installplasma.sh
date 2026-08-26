8e8f4ea8692d145b94c5a3eb529e8608479c748c
coolnotlight3 <nonexistient@gmail.com>
2026-08-26 18:56:23 -0400

    Update installplasma.sh

--- installplasma.sh
+++ installplasma.sh
@@ -1,4 +1,4 @@
-sudo xbps-install kde-plasma kde-baseapps sddm xorg pulseaudio NetworkManager
+sudo xbps-install kde-plasma kde-baseapps sddm xorg pipewire wireplumber NetworkManager
 sudo ln -s /etc/sv/sddm /var/service
 sudo ln -s /etc/sv/dbus /var/service
 sudo ln -s /etc/sv/NetworkManager /var/service
